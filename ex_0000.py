2d8f8737df867bf4925f51331fda99f7b12b0c0d
Bigphess <xrp0308@gmail.com>
2018-12-17 18:22:24 +0900

    ex 0000

--- ex_0000.py
+++ ex_0000.py
@@ -3,7 +3,7 @@ import matplotlib.image as mpimg
 import numpy as np 
 
 image = mpimg.imread('kabi.png')
-plt.text(250,80,'4',fontsize = 80, color = 'red')
+plt.text(250,80,'4',fontsize = 60, color = 'black', bbox={'facecolor':'red', 'alpha':0.8, 'boxstyle':'round'})
 
 plt.imshow(image)
 # plt.axis('off')
